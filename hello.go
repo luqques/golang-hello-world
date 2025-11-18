e1406b6e4e5835b720ff340be4d5068f9ed379bc
Lucas da Cunha <luques.cunha@gmail.com>
2025-11-17 22:41:14 -0300

    implementando for

--- hello.go
+++ hello.go
@@ -7,18 +7,8 @@ import (
 )
 
 func main() {
-	arraySites := criarArray()
-	fmt.Println(arraySites)
-
-	sliceSites := criarSlice()
-	fmt.Println(sliceSites)
-	fmt.Println("Capacidade do slice dobrada:", cap(sliceSites))
-
 	exibeMenu()
 	comando := lerComando()
-
-	nome, idade := devolveNomeEIdade()
-	fmt.Println("Ola", nome, "sua idade é", idade)
 
 	switch comando {
 	case 1:
@@ -32,12 +22,6 @@ func main() {
 		fmt.Println("Comando não reconhecido...")
 		os.Exit(-1)
 	}
-}
-
-func devolveNomeEIdade() (string, int) {
-	nome := "Lucas"
-	idade := 21
-	return nome, idade
 }
 
 func exibeMenu() {
@@ -57,29 +41,29 @@ func lerComando() int {
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 
-	site := "https://httpbin.org/status/404" // ou 200
-	resp, _ := http.Get(site)
+	sliceSites := criarSitesSlice()
+	fmt.Println(sliceSites)
 
-	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso!")
-	} else {
-		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	for i := range sliceSites {
+		monitorarSite(sliceSites[i])
 	}
 }
 
-func criarArray() [5]string {
-	var sites [5]string
-	sites[0] = "https://www.alura.com.br"
-	sites[1] = "https://www.caelum.com.br"
-	sites[2] = "https://www.google.com.br"
-	sites[3] = "https://www.github.com"
-	sites[4] = "https://www.stackoverflow.com"
-
-	return sites
+func monitorarSite(site string) {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao acessar o site:", site)
+	} else {
+		if resp.StatusCode == 200 {
+			fmt.Println("Site:", site, "foi carregado com sucesso!")
+		} else {
+			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		}
+	}
 }
 
-func criarSlice() []string {
-	sites := []string{"https://www.alura.com.br", "https://www.caelum.com.br", "https://www.google.com.br", "https://www.github.com", "https://www.stackoverflow.com"}
+func criarSitesSlice() []string {
+	sites := []string{"https://www.alura.com.br", "https://www.google.com.br", "https://www.github.com", "https://www.stackoverflow.com"}
 	sites = append(sites, "https://www.udemy.com")
 
 	return sites
